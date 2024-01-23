8d071e0bfdb14617eac5c9a3212e17e00e7ff4ae
Wesley Kwong <136935666+WestKwong@users.noreply.github.com>
2024-01-23 12:30:28 -0500

    Add files via upload
    included about page
    new styles for about page
    modifications on image folder

--- script.js
+++ script.js
@@ -7,3 +7,54 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 } 
+
+
+
+
+
+
+
+
+// slideshow
+
+
+function plusSlides(n) {
+  clearTimeout(); // Clear automatic slideshow timeout
+  showSlidesManually(slideIndex += n);
+}
+
+function currentSlide(n) {
+  clearTimeout(); // Clear automatic slideshow timeout
+  showSlidesManually(slideIndex = n);
+}
+
+function showSlidesManually(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+
+
+          // auto slides
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000);
+} 
+
+
